b0fbc2705f59d6250dbd28deef8d54a55d2eb20f
drewkees <baldonado0514@gmail.com>
2026-01-14 16:24:32 +0800

    Fix contact ai

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -48,7 +48,7 @@ const Projects = () => {
       category: "Web Development",
       icon: <Zap className="h-6 w-6" />,
       liveUrl: "https://www.ipon365.site/",
-      githubUrl: "https://github.com/andrewbaldonado/ai-automatiosn",
+      githubUrl: "https://github.com/andrewbaldonado/ai-automatiosFGGn",
       featured: true,
       image: ipon365,
     },
